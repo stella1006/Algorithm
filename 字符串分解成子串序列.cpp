8cddf37cb75b40922d0a1c275f5cc6c181f71ad6
stella1006 <stella.lee.lzr@gmail.com>
2016-01-23 21:26:44 +0800

    字符串分解成子串序列

---
+++ 字符串分解成子串序列.cpp
@@ -0,0 +1,42 @@
+﻿//字符串分解成子串序列
+
+#include <stdio.h>
+#include <string>
+#include <algorithm>
+#include <cctype>
+#include <iostream>
+#include <vector>
+#include <cassert>
+#include <list>
+using namespace std;
+
+list<string> getListofWords(string &s) {
+	list<string> result;
+	char spaces[] = " ,.!?";
+	char* cs;
+	cs = strtok((char*)(s.c_str()), " ");
+	while (cs != NULL) {
+		result.push_back(cs);
+		cs = strtok(NULL, " ");
+		
+	}
+	return result;
+
+}
+
+
+int main() {
+	string s;
+	getline(cin, s, '\n');
+	list<string> result = getListofWords(s);
+	for (list<string>::iterator i = result.begin(); i != result.end(); i++) {
+		cout << *i << endl;
+	}
+	
+	
+	
+	
+
+	system("pause");
+	return 0;
+}
