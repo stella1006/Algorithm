9d32a8f7ab77008e5999d80d43873bca1e2d35f8
stella1006 <stella.lee.lzr@gmail.com>
2016-01-23 12:17:17 +0800

    adjf

---
+++ 1350给出每个点出度为1的有向图求环的个数（简单）.cpp
@@ -0,0 +1,50 @@
+﻿#include <iostream>
+#include <cstring>
+using namespace std;
+int c[1000001];
+int bank[1000001];
+
+int main() {
+	int N, p;
+	while(cin >> N) {
+	
+	int color = 1;
+	int count = 0;
+	
+	
+	memset(c, 0, sizeof(c));
+	for (int i = 1; i <= N; i++) {
+		cin >> bank[i];
+		if (bank[i] == i) {
+			
+			count++;
+			c[i] = color++;
+		}
+	}
+	
+	for (int i = 1; i<= N; i++) {
+		if (c[i] == 0) {
+			color++;
+			p = i;
+			
+			while(c[p] == 0 ) {
+				c[p] = color;
+				p = bank[p];
+				if (c[p] == color) {
+					count++;
+					
+				}
+			}
+			
+
+
+
+		}
+
+	}
+
+	cout << count << endl;
+	}
+	system("pause");
+	return 0;
+}
