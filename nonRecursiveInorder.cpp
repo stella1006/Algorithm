1ff67db32cd8313e43c96a96cd11c0b11928b4db
stella1006 <stella.lee.lzr@gmail.com>
2016-04-23 19:07:08 +0800

    nonRecursiveInorder

---
+++ nonRecursiveInorder.cpp
@@ -0,0 +1,46 @@
+#include <cstdio>
+#include <cstring>
+#include <string>
+#include <cmath>
+#include <iostream>
+#include <vector>
+#include <queue>
+#include <iomanip>
+#include <algorithm>
+#include <set>
+#include <map>
+#include <stack>
+#define LL long long
+#define ULL unsigned long long 
+#define INF 1<<30 
+#define Mod 1000000007
+#define PII pair<int,int>
+#define mk make_pair
+#define Min(a,b) (a<b?a:b)
+#define Max(a,b) (a<b?b:a)
+using namespace std;
+
+struct BinaryNode {
+	int elements;
+	BinaryNode* left;
+	BinaryNode* right;
+};
+
+void nonRecursiveInorder(BinaryNode* root) {
+	stack<BinaryNode* > s;
+	BinaryNode* p = root;
+	while (p || !s.empty()) {
+		if (p) {
+			s.push(p);
+			p = p->left;
+		} else {
+			p = s.top();
+			cout << p->elements;
+			p = p->right;
+		}
+	}
+}
+
+int main() {
+	return 0;
+}
