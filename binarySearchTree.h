fe1d09187f0b0a190b2d6768abb88749c2262d19
stella1006 <stella.lee.lzr@gmail.com>
2016-05-09 17:55:10 +0800

    modify binTree

--- binarySearchTree.h
+++ binarySearchTree.h
@@ -22,20 +22,21 @@ public:
 	BinTree(const BinTree& original);   //copy constructor
 	BinTree & operator = (const BinTree<T> &original);   //override = operator
 	~BinTree();   //destructor
-	bool insert(const T d);       //insert data d, if succeed, return true and return false otherwise
-	bool search(const T d);       //find d. Return true if succeed
-	void print() const;        //level traverse
-	void traversal() const;   //traverse inorder
+	bool insert(const T d) ;       //insert data d, if succeed, return true and return false otherwise
+	bool search(const T d) const;       //find d. Return true if succeed
+	void print() ;        //level traverse
+	void traversal(void (*visit)(T d)) const;   //traverse inorder
 
 private:
 	treeNode<T>* root;
 	int count;    //number of nodes
 	bool insertR(treeNode<T>* & t, const T d); //insert recursivly
-	void printR(treeNode<T>*  t) const;
-	//void levelTraR(const treeNode<T>* & t, int level, vector<vector<int>> &res);
+	void printR(treeNode<T>* & t) ;
+	void levelTraR(treeNode<T>* & t, int level, vector<vector<T>> &res);
 	void release(treeNode<T> *t);
-	void traversalR(const treeNode<T>*t) const;
+	void traversalR(const treeNode<T>*t, void (*visit)(T d)) const;
 };
+
 
 template <typename T> 
 bool BinTree<T>::insertR(treeNode<T>* & t, const T d) {
@@ -54,21 +55,30 @@ bool BinTree<T>::insertR(treeNode<T>* & t, const T d) {
 	return false;
 }
 
-//template <typename T> 
-//void BinTree<T>::levelTraR(const treeNode<T>* & t, int level, vector<vector<int>> &res) {
-//	if (!t) return;
-//	if (level > res.size()) {
-//		res.push_back(vector<int>());
-//	}
-//	res[level-1].push_back(t->data);
-//	levelTraR(t->left, level+1, res);
-//	levelTraR(t->right, level+1, res);
-//
-//}
+template <typename T> 
+void BinTree<T>::levelTraR(treeNode<T>* & t, int level, vector<vector<T>> &res) {
+	if (!t) return;
+	if (level > res.size()) {
+		res.push_back(vector<int>());
+	}
+	res[level-1].push_back(t->data);
+	levelTraR(t->left, level+1, res);
+	levelTraR(t->right, level+1, res);
+
+}
 
 template <typename T> 
-void BinTree<T>::printR(treeNode<T>* t) const {
-	queue<treeNode<T>*> q;
+void BinTree<T>::printR(treeNode<T>* & t)  {
+	vector<vector<T>> res;
+	levelTraR(t, 1, res);
+	for (int i = 0; i < res.size(); i++) {
+		for (int j = 0; j < res[i].size(); j++) {
+			cout << res[i][j] << " ";
+		}
+		cout << endl;
+	}
+
+	/*queue<treeNode<T>*> q;
 	if (t) q.push(t);
 	else {
 		cout << "empty" << endl;
@@ -82,7 +92,7 @@ void BinTree<T>::printR(treeNode<T>* t) const {
 			q.push(temp->left);
 		if (temp->right) 
 			q.push(temp->right);
-	}
+	}*/
 	cout << endl;
 }
 
@@ -92,11 +102,11 @@ void BinTree<T>::release(treeNode<T> *t) {
 }
 
 template <typename T> 
-void BinTree<T>::traversalR(const treeNode<T>*t) const {
+void BinTree<T>::traversalR(const treeNode<T>*t, void (*visit)(T d)) const {
 	if (t != NULL) {
-		traversalR(t->left);
-		cout << t->data << " ";
-		traversalR(t->right);
+		traversalR(t->left, visit);
+		visit(t->data);
+		traversalR(t->right, visit);
 	}
 }
 
@@ -109,6 +119,12 @@ BinTree<T>::BinTree(const BinTree& original) {
 template <typename T>
 BinTree<T> & BinTree<T>::operator = (const BinTree<T> &original) {
 	root = &original->root;
+	if (root != NULL) {
+		root->left = original->root->left;
+		root->right = original->root->right;
+	}
+	
+
 	return *this;
 }
 
@@ -125,7 +141,7 @@ bool BinTree<T>::insert(const T d) {
 }
 
 template <typename T>
-bool BinTree<T>::search(const T d) {
+bool BinTree<T>::search(const T d) const{
 	treeNode<T>* p = root;
 	while (p != NULL) {
 		if (d < p->data) {
@@ -140,13 +156,13 @@ bool BinTree<T>::search(const T d) {
 }
 
 template <typename T>
-void BinTree<T>::print() const {
+void BinTree<T>::print() {
 	printR(root);
 }
 
 template <typename T>
-void BinTree<T>::traversal() const {
-	traversalR(root);
+void BinTree<T>::traversal(void (*visit)(T d)) const {
+	traversalR(root, visit);
 	cout << endl;
 }
 
